65498322f1a042a29f7d9c8c246deb2aae248fd2
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2023-10-08 12:37:10 +0530

    add recon + forecast heads, fix n_heads dataclass annotation

--- src/model.py
+++ src/model.py
@@ -19,7 +19,7 @@ from .patches import PatchEmbedding
 class ModelConfig:
     patch_len: int = 16
     d_model: int = 128
-    n_heads: 4
+    n_heads: int = 4
     n_layers: int = 4
     ffn_dim: int = 256
     dropout: float = 0.1
@@ -55,3 +55,68 @@ class TSEncoder(nn.Module):
         x = self.embed(patches)            # (B, N, D)
         x = self.encoder(x)                # (B, N, D)
         return self.norm(x)
+
+
+class ReconstructionHead(nn.Module):
+    """maps each token back to a patch (for masked patch reconstruction)."""
+
+    def __init__(self, d_model: int, patch_len: int):
+        super().__init__()
+        self.proj = nn.Linear(d_model, patch_len)
+
+    def forward(self, h: torch.Tensor) -> torch.Tensor:
+        # h: (B, N, D) -> (B, N, P)
+        return self.proj(h)
+
+
+class ForecastingHead(nn.Module):
+    """flattens encoder outputs and projects to a forecast horizon."""
+
+    def __init__(self, d_model: int, n_patches: int, horizon: int,
+                 dropout: float = 0.1):
+        super().__init__()
+        self.flatten = nn.Flatten(start_dim=1)
+        self.dropout = nn.Dropout(dropout)
+        self.proj = nn.Linear(d_model * n_patches, horizon)
+
+    def forward(self, h: torch.Tensor) -> torch.Tensor:
+        # h: (B, N, D) -> (B, horizon)
+        x = self.flatten(h)
+        x = self.dropout(x)
+        return self.proj(x)
+
+
+class TSFoundationModel(nn.Module):
+    """encoder + recon head; used for pretraining."""
+
+    def __init__(self, cfg: ModelConfig):
+        super().__init__()
+        self.encoder = TSEncoder(cfg)
+        self.recon = ReconstructionHead(cfg.d_model, cfg.patch_len)
+
+    def forward(self, patches: torch.Tensor) -> torch.Tensor:
+        h = self.encoder(patches)
+        return self.recon(h)
+
+
+class TSForecaster(nn.Module):
+    """encoder + forecasting head; used for finetuning."""
+
+    def __init__(self, cfg: ModelConfig, n_patches: int, horizon: int):
+        super().__init__()
+        self.encoder = TSEncoder(cfg)
+        self.head = ForecastingHead(cfg.d_model, n_patches, horizon,
+                                    dropout=cfg.dropout)
+
+    def forward(self, patches: torch.Tensor) -> torch.Tensor:
+        h = self.encoder(patches)
+        return self.head(h)
+
+    def load_pretrained_encoder(self, ckpt_path: str,
+                                map_location: str = "cpu") -> None:
+        sd = torch.load(ckpt_path, map_location=map_location)
+        if "encoder" in sd:
+            self.encoder.load_state_dict(sd["encoder"])
+        else:
+            # try loading a flat encoder state dict
+            self.encoder.load_state_dict(sd)
